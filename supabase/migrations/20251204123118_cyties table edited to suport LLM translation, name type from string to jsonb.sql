5a817b6dbf6dff7e508d3758ece8392f11fce1dc
f1c4 <f1c4@live.com>
2025-12-04 13:34:49 +0100

    Forgot to add supabase migration fiel to last refactor

---
+++ supabase/migrations/20251204123118_cyties table edited to suport LLM translation, name type from string to jsonb.sql
@@ -0,0 +1,29 @@
+drop index if exists "public"."cities_name_country_id_key";
+
+alter table "public"."cities" alter column "name" set data type jsonb using "name"::jsonb;
+
+CREATE UNIQUE INDEX cities_name_country_id_key ON public.cities USING btree (name, country_id);
+
+drop policy "DEV-ONLY: Allow all authenticated users full access to insuranc" on "storage"."objects";
+
+
+  create policy "DEV-ONLY: Allow  users full access to insurances"
+  on "storage"."objects"
+  as permissive
+  for all
+  to public
+using (((bucket_id = 'patient-insurances'::text) AND (auth.role() = 'authenticated'::text)))
+with check (((bucket_id = 'patient-insurances'::text) AND (auth.role() = 'authenticated'::text)));
+
+
+
+  create policy "DEV-ONLY: Allow all users full access"
+  on "storage"."objects"
+  as permissive
+  for all
+  to public
+using (((bucket_id = 'patient-id-documents'::text) AND (auth.role() = 'authenticated'::text)))
+with check (((bucket_id = 'patient-id-documents'::text) AND (auth.role() = 'authenticated'::text)));
+
+
+
